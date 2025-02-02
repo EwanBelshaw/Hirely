eedda4a871f7f0450b734f3eb561dedcf17a8939
itsminani <minanihertierluc@gmail.com>
2025-02-02 03:58:40 -0500

    Solved issues with

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
+// Ensure this file is saved with the .js extension
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
